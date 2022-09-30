600283543b77dfcfd6fabc2a73e00da5c08e7f0d
venkateswarlu <chvenkateswarlu885@gmail.com>
2022-09-30 13:23:03 +0530

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,28 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Signup from './components/Signup';
+import Auth from './components/Auth';
+import AdminNavbar from './components/AdminNavbar';
+import AddAcademy from './components/AddAcademy';
+import AddCourse from './components/AddCourse';
+import AddStudent from './components/AddStudent';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth/>}/>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/navbar" element={<AdminNavbar/>} />
+        <Route path='/adminAcademy' element={<AddAcademy/>} />
+        <Route path='/adminCourse' element={<AddCourse />} />
+        {/* <Route path='/adminStudents' element={<AdminStudents/>} /> */}
+        <Route path='/adminStudent' element={<AddStudent/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
